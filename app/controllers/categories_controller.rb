bb49a8c3fe39185c22b5d9e5ff1cef7b8647e776
Danya Kim <itsdanya@gmail.com>
2014-12-27 16:11:19 +0300

    Render JSON for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,10 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
-    render json: @categories
+    render formats: :json
   end
 
   def show
     @category = Category.find(params[:id])
-    render json: @category.to_json(include: :topics)
   end
 end
