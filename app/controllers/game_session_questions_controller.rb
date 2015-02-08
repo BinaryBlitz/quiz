09694d7b8fb304381f75298c61529f3d224390ff
Danya Kim <itsdanya@gmail.com>
2015-02-08 12:03:29 +0300

    Refactor GameSessionQuestion update action

--- app/controllers/game_session_questions_controller.rb
+++ app/controllers/game_session_questions_controller.rb
@@ -1,54 +1,68 @@
 class GameSessionQuestionsController < ApplicationController
-  def show
-    @game_session_question = GameSessionQuestion.find(params[:id])
-    render json: @game_session_question
-  end
+  before_action :find_session_question, only: [:show, :update]
+
+  # def show
+  #   render json: @session_question
+  # end
 
   def update
-    @game_session_question = GameSessionQuestion.find(params[:id])
-    if current_player == @game_session_question.game_session.host
-      @game_session_question.host_answer_id = params[:game_session_question][:answer_id]
-      @game_session_question.host_time = params[:game_session_question][:time]
-    elsif current_player == @game_session_question.game_session.opponent
-      @game_session_question.opponent_answer_id = params[:game_session_question][:answer_id]
-      @game_session_question.opponent_time = params[:game_session_question][:time]
-    end
+    @current_session = @session_question.game_session
+    answer_id = params[:game_session_question][:answer_id]
+    time = params[:game_session_question][:time]
 
-    if @game_session_question.save
-      push_answer
+    update_answer(answer_id, time)
+    if @session_question.save
       head :no_content
     else
-      render json: @game_session_question.errors, status: :unprocessable_entity
+      render json: @session_question.errors, status: :unprocessable_entity
     end
   end
 
   private
 
-  def push_answer
-    current_session = @game_session_question.game_session
-    # If host updated answer, send data to opponent
-    if @current_player == current_session.host
-      Pusher.trigger(
-        "player-session-#{current_session.opponent_id}",
-        'opponent-answer',
-        answer_data(
-          @game_session_question.id,
-          @game_session_question.host_answer_id,
-          @game_session_question.host_time)
-        )
-    elsif @current_player == current_session.opponent
-      Pusher.trigger(
-        "player-session-#{current_session.host_id}",
-        'opponent-answer',
-        answer_data(
-          @game_session_question.id,
-          @game_session_question.opponent_answer_id,
-          @game_session_question.opponent_time))
+  # Update answer data (host / opponent)
+  def update_answer(answer_id, time)
+    if current_player == @current_session.host
+      update_host_answer(answer_id, time)
+      push_answer(@current_session.opponent, answer_id, time)
+    elsif current_player == @current_session.opponent
+      update_opponent_answer(answer_id, time)
+      push_answer(@current_session.host, answer_id, time)
     end
   end
 
-  def answer_data(session_question_id, answer_id, answer_time)
-    { game_session_question_id: session_question_id, answer_id: answer_id, answer_time: answer_time }
+  # Push to host
+  def update_host_answer(answer_id, time)
+    @session_question.host_answer_id = answer_id
+    @session_question.host_time = time
+  end
+
+  # Push to opponent
+  def update_opponent_answer(answer_id, time)
+    @session_question.opponent_answer_id = answer_id
+    @session_question.opponent_time = time
+  end
+
+  # Push player answer to opponent
+  def push_answer(player, answer_id, time)
+    Pusher.trigger(
+      "player-session-#{player.id}",
+      'opponent-answer',
+      answer_data(answer_id, time)
+    )
+  end
+
+  # Format data for pusher
+  def answer_data(answer_id, answer_time)
+    {
+      game_session_question_id: @session_question.id,
+      answer_id: answer_id,
+      answer_time: answer_time
+    }
+  end
+
+  def find_session_question
+    @session_question = GameSessionQuestion.find(params[:id])
   end
 
   def game_session_question_params
