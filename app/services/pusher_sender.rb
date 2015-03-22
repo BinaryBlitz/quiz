a89863b50635881b494785285d18311ec5e1719e
Danya Kim <itsdanya@gmail.com>
2015-03-22 12:37:49 +0300

    Use Rails logger in PusherSender

--- app/services/pusher_sender.rb
+++ app/services/pusher_sender.rb
@@ -4,14 +4,14 @@ class PusherSender
   end
 
   def start_game
-    @players_ids.each{ |id| start_game_for_player(id) }
+    @players_ids.each { |id| start_game_for_player(id) }
   end
 
   private
 
   def start_game_for_player(player_id)
-    logger.debug "#{Time.zone.now}: Try to sent event to player #{player_id}."
+    Rails.logger.debug "#{Time.zone.now}: Try to sent event to player #{player_id}."
     Pusher.trigger("player-session-#{player_id}", 'game-start', {})
-    logger.debug "#{Time.zone.now}: Game start event sent to player #{player_id}."
+    Rails.logger.debug "#{Time.zone.now}: Game start event sent to player #{player_id}."
   end
 end
