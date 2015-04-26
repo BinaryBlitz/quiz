15a097b8ee3b32a98cc115882ac7adb2077f0f4b
Danya Kim <itsdanya@gmail.com>
2015-04-26 20:54:03 +0300

    Set up mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.smtp_settings = {
+    :port           => Rails.application.secrets.mailgun_smtp_port,
+    :address        => Rails.application.secrets.mailgun_smtp_server,
+    :user_name      => Rails.application.secrets.mailgun_smtp_login,
+    :password       => Rails.application.secrets.mailgun_smtp_password,
+    :domain         => 'partyapp.herokuapp.com',
+    :authentication => :plain,
+  }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'quizapp.binaryblitz.ru' }
 end
