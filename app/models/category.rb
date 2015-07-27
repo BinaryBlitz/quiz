02cfdf3b5a1fb32d5a12124db3b7ebe55e18cfdb
Dan K <itsdanya@gmail.com>
2015-07-27 14:50:16 +0300

    Add has_many constraint to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,6 +12,7 @@
 
 class Category < ActiveRecord::Base
   has_many :topics, dependent: :destroy
+  has_many :topic_results, dependent: :destroy
 
   mount_uploader :background, BackgroundUploader
   mount_uploader :banner, BackgroundUploader
