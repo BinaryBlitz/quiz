f62920773d2b65a435bb147f7de73f1610230dc6
mendable <yanberdint@gmail.com>
2015-09-07 18:36:58 +0300

    increase backlog

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ old_pid    = pid_file + '.oldbin'
 
 timeout 15
 worker_processes 1
-listen socket_file, :backlog => 1024
+listen socket_file, backlog: 2048
 pid pid_file
 stderr_path err_log
 stdout_path log_file
