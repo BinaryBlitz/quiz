c1ff979fa164602e14012ff5e01195593aa8600a
Dan K. <itsdanya@gmail.com>
2015-09-12 00:04:03 +0300

    Reversible migrations

--- db/migrate/20150720210009_add_platform_to_device_tokens.rb
+++ db/migrate/20150720210009_add_platform_to_device_tokens.rb
@@ -1,6 +1,6 @@
 class AddPlatformToDeviceTokens < ActiveRecord::Migration
   def change
-    remove_column :device_tokens, :android
+    remove_column :device_tokens, :android, :boolean
     add_column :device_tokens, :platform, :string
   end
 end
