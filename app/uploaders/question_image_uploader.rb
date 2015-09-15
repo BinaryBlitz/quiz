371024935e4f17d8ee7ecd0b10fc64d09af23aad
Dan K <itsdanya@gmail.com>
2015-09-15 19:00:20 +0300

    Compress category banners and backgrounds

--- app/uploaders/question_image_uploader.rb
+++ app/uploaders/question_image_uploader.rb
@@ -1,6 +1,5 @@
 class QuestionImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -15,20 +14,17 @@ class QuestionImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fit: [600, 600]
+  process resize_to_limit: [600, 600]
   process convert: 'jpg'
   process quality: 80
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [100, 100]
+    process resize_to_fit: [150, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
