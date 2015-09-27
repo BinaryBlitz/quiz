801f2328bde80898df5e850b7bf065df45a41a16
Dan K <itsdanya@gmail.com>
2015-09-27 23:04:39 +0300

    Move from low-level caching to fragment caching

--- app/models/concerns/player_topics.rb
+++ app/models/concerns/player_topics.rb
@@ -2,10 +2,8 @@ module PlayerTopics
   extend ActiveSupport::Concerns
 
   def favorite_topic
-    Rails.cache.fetch("#{id}/favorite_topic", expires_in: 24.hours) do
-      topic_result = topic_results.order(count: :desc).first
-      topic_result.topic if topic_result
-    end
+    topic_result = topic_results.order(count: :desc).first
+    topic_result.topic if topic_result
   end
 
   def favorite_topic_games
@@ -13,19 +11,15 @@ module PlayerTopics
   end
 
   def favorite_topics
-    Rails.cache.fetch("#{id}/favorite_topics", expires_in: 24.hours) do
-      topic_results.order(count: :desc).limit(3).map(&:topic)
-    end
+    topic_results.order(count: :desc).limit(3).map(&:topic)
   end
 
   def friends_favorite_topics
-    Rails.cache.fetch("#{id}/friends_favorite_topics", expires_in: 24.hours) do
-      topics = []
-      friends.sort_by(&:favorite_topic_games).each do |player|
-        topic = player.favorite_topic
-        topics << topic if !topics.include?(topic) && topic
-      end
-      topics.first(3)
+    topics = []
+    friends.sort_by(&:favorite_topic_games).each do |player|
+      topic = player.favorite_topic
+      topics << topic if !topics.include?(topic) && topic
     end
+    topics.first(3)
   end
 end
